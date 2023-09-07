fde9d35b66d680e16d2f5e7b8059000751fedbd8
alekseialenkin <124128504+alekseialenkin@users.noreply.github.com>
2023-09-07 20:23:09 +0700

    notFound and notOwn getWithUser Tests

--- src/test/java/ru/javawebinar/topjava/service/mealservicetests/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/mealservicetests/datajpa/DataJpaMealServiceTest.java
@@ -1,11 +1,13 @@
 package ru.javawebinar.topjava.service.mealservicetests.datajpa;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.mealservicetests.MealServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import static ru.javawebinar.topjava.Profiles.DATAJPA;
 
@@ -13,11 +15,19 @@ import static ru.javawebinar.topjava.Profiles.DATAJPA;
 public class DataJpaMealServiceTest extends MealServiceTest {
 
     @Test
-    public void getMealWithUser() {
+    public void getWithUser() {
         Meal m = service.getWithUser(MealTestData.USER_MEAL_ID, UserTestData.USER_WITH_MEAL_ID);
         MealTestData.MEAL_MATCHER.assertMatch(m, MealTestData.userMeal1);
         UserTestData.USER_MATCHER.assertMatch(m.getUser(), UserTestData.userWithMeal);
     }
 
+    @Test
+    public void getWithUserNotFound() {
+        Assert.assertThrows(NotFoundException.class, () -> service.getWithUser(MealTestData.NOT_FOUND, UserTestData.USER_WITH_MEAL_ID));
+    }
 
+    @Test
+    public void getWithUserNotOwn() {
+        Assert.assertThrows(NotFoundException.class, () -> service.getWithUser(MealTestData.USER_MEAL_ID, UserTestData.ADMIN_ID));
+    }
 }
