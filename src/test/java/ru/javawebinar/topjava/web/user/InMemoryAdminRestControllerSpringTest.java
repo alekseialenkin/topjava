fbc57a428aec14b3ccd5e7408141f45a82687a80
alekseialenkin <124128504+alekseialenkin@users.noreply.github.com>
2023-10-24 20:13:29 +0700

    deleted enable method test

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -8,7 +8,8 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import static ru.javawebinar.topjava.UserTestData.*;
+import static ru.javawebinar.topjava.UserTestData.NOT_FOUND;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @SpringJUnitConfig(locations = {"classpath:spring/inmemory.xml"})
 class InMemoryAdminRestControllerSpringTest {
@@ -33,11 +34,5 @@ class InMemoryAdminRestControllerSpringTest {
     @Test
     void deleteNotFound() {
         Assertions.assertThrows(NotFoundException.class, () -> controller.delete(NOT_FOUND));
-    }
-
-    @Test
-    void enable() {
-        controller.enable(ADMIN_ID, false);
-        Assertions.assertFalse(controller.get(ADMIN_ID).isEnabled());
     }
 }
