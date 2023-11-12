b5545698a9da9ce90d048887f87e71f5f050af6b
alekseialenkin <124128504+alekseialenkin@users.noreply.github.com>
2023-11-12 15:11:49 +0700

    added custom validator for registration form

---
+++ src/main/java/ru/javawebinar/topjava/web/validator/EmailDuplicateValidator.java
@@ -0,0 +1,44 @@
+package ru.javawebinar.topjava.web.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
+import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.to.UserTo;
+import ru.javawebinar.topjava.util.UsersUtil;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+//https://habr.com/ru/articles/424819/
+@Component
+public class EmailDuplicateValidator implements Validator {
+    @Autowired
+    private UserService service;
+
+    @Autowired
+    @Qualifier("validator")
+    private LocalValidatorFactoryBean validatorFactory;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return UserTo.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        SpringValidatorAdapter validatorAdapter = new SpringValidatorAdapter(validatorFactory);
+        validatorAdapter.validate(target, errors);
+        UserTo user = (UserTo) target;
+        UserTo user1 = null;
+        try {
+            user1 = UsersUtil.asTo(service.getByEmail(user.getEmail()));
+        } catch (NotFoundException ignored) {
+        }
+        if (user1 != null && user.getEmail().equals(user1.getEmail())) {
+            errors.rejectValue("email", "email.duplicate");
+        }
+    }
+}
